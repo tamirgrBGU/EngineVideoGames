7f47bf644477bc6f2126c00a245d976ff85a1658
Roy Hersh <royhersh@post.bgu.ac.il>
2019-08-04 10:21:10 +0300

    merge

--- Game/Menu.h
+++ Game/Menu.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "display.h"
-#include "..\res\includes\imgui\imgui.h"
-#include"..\res\includes\imgui\imgui_impl_glfw_gl3.h"
+#include "imgui\imgui.h"
+#include "imgui\imgui_impl_glfw_gl3.h"
 #include "scene.h"
 
 class Menu
